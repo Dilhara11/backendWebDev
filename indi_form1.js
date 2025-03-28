e1bd8afeb3f427b4a9b736c031bd47cb9b9e00de
A.P.A.K.DILHARA <achinthakaveen11@gmail.com>
2025-03-28 11:52:36 +0530

    individual_practices

---
+++ indi_form1.js
@@ -0,0 +1,45 @@
+const http = require('http');
+
+const querystring = require('querystring');
+
+const server = http.createServer((req, res) =>{
+    if(req.method == 'POST'){
+        let body = '';
+
+        req.on('data', chunk => {
+            body += chunk;
+        });
+
+        req.on('end', () => {
+            const parsedBody = querystring.parse(body);
+
+            res.writeHead(200, {"content-type" : "text/html"});
+
+            res.end(`Hello, ${parsedBody.name} and your email is ${parsedBody.email}!`);
+        });
+    } else {
+        res.writeHead(200, {"content-type":"text/html"});
+
+        res.end(
+            `<!DOCTYPE html>
+             <html lang="en">
+             <head>
+                 <meta charset="UTF-8">
+                 <title>Node.js Form Example</title>
+             </head>
+             <body>
+                 <form method="POST">
+                     Name: <input type="text" name="name" placeholder="Enter your name">
+                     <br>
+                     Email: <input type = "email" name = "email" placeholder = "example@gmial.com">
+                     <input type="submit" value="Submit">
+                 </form>
+             </body>
+             </html>`
+        );
+    }
+});
+
+server.listen(80, () => {
+    console.log('Server Running at http://localhost: 80');
+});
